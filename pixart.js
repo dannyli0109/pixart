afea3a7342ff0dd6bbba448f74c953ce693019b5
Danny Li <dannyli0109@gmail.com>
2017-07-31 15:37:53 +1000

    added 20 squares

--- pixart.js
+++ pixart.js
@@ -1,6 +1,16 @@
 var $colorInput = $("#color-field")
+var $body = $("body")
+
+// change color
 $("#set-color").on("click", function(event){
   event.preventDefault()
   $(".brush").css("background", $colorInput.val());
 })
+
+// create squares
+for (var i = 0; i < 20; i++) {
+  $body.append($("<div class='brush'>"))
+}
+
+
 // enter key will work as well
