be8f2f93122d8dd8d3af70763d4bfde9f91e6bc3
Danny Li <dannyli0109@gmail.com>
2017-07-31 15:50:48 +1000

    mouse over to change color

--- pixart.js
+++ pixart.js
@@ -1,21 +1,23 @@
 var $colorInput = $("#color-field")
 var $body = $("body")
+var currentColor = $(".brush").css("background")
 
 // change color
 $("#set-color").on("click", function(event){
   event.preventDefault()
   $(".brush").css("background", $colorInput.val());
+  currentColor = $colorInput.val()
 })
 
 // create squares
 for (var i = 0; i < 20; i++) {
-  $body.append($("<div class='brush'>"))
+  $body.append($("<div class='square'>"))
 }
 
 // change color of the square on click
-$body.on("click", function(event){
-  if (event.target.className == "brush") {
-    event.target.style.background = $colorInput.val()
+$body.on("mouseover", function(event){
+  if (event.target.className == "square") {
+    event.target.style.background = currentColor
   }
 })
 
